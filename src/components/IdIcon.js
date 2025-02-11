02e250769acdf3f2efdde9142a13853a9e61077a
officialevsty <officialevsty@Ubuntu.myguest.virtualbox.org>
2025-02-11 16:59:25 +0300

    end with login page (without animations)

--- src/components/IdIcon.js
+++ src/components/IdIcon.js
@@ -2,12 +2,15 @@ import "../styles/idIcon.css"
 
 const IDIcon = () => {
     return (
-        <div className="icon-wrapper">
+        /*<div className="icon-wrapper">
             <p>net</p>
             <div className="gradient-icon">
                 <p className="icon-text">ID</p>
             </div>
-        </div>
+        </div>*/
+        <object className='logo-icon' data="/assets/svg/Logo%20ID.svg" type="image/svg+xml">
+            <img src="../../public/assets/svg/Logo%20ID.svg" alt="" />
+        </object>
     )
 }
 
--- src/components/LoginModal.js
+++ src/components/LoginModal.js
@@ -17,15 +17,26 @@ const LoginForm = () => {
                     <p className='login-title'>Войдите в систему</p>
                 </div>
                 <div className='login-inputs'>
-                    <input type='text' placeholder='Введите email' className='login-input' {...register('email', { required: "Введите email", pattern: { value: /^\S+@\S+$/i, message: "Некорректный email"}})}/>
-                    {errors.email && <p className='email-error'>{errors.email.message}</p>}
-                    <input type='text' placeholder='Введите пароль' className='login-input' {...register('password', { required: "Введите пароль", minLength: { value: 8, message: "Минимальная длина пароля - 8 символов"}})}/>
-                    {errors.password && <p className='email-error'>{errors.password.message}</p>}
-                </div>
-                <div className='login-buttons'>
-                    <button className='form-btn login' type='submit'>Войти</button>
+                    <div className='input-wrapper'>
+                        <input id='email' type='text' placeholder=' ' className='login-input' {...register('email', {
+                            required: "",
+                            pattern: {value: /^\S+@\S+$/i, message: "Некорректный email"}
+                        })}/>
+                        <label htmlFor='email' className='placeholder'>Введите email</label>
+                    </div>
+
+                {errors.email && <p className='email-error'>{errors.email.message}</p>}
+                <input id='password' type='text' placeholder=' ' className='login-input' {...register('password', {
+                    required: "",
+                    minLength: {value: 8, message: "Минимальная длина пароля - 8 символов"}
+                })}/>
+                {errors.password && <p className='email-error'>{errors.password.message}</p>}
+                <label htmlFor='password' className='placeholder'>Введите пароль</label>
+            </div>
+            <div className='login-buttons'>
+                <button className='form-btn login' type='submit'>Войти</button>
                     <button className='form-btn register'>Зарегистрироваться</button>
-                    <a className='form-ref' href="/*">Не помню пароль</a>
+                    <button className='form-btn forgot'>Не помню пароль</button>
                 </div>
             </div>
         </form>
