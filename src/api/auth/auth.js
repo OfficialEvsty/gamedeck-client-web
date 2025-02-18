5d3f3f77a5d8f06d8b01e5474519444bbf3d1bfe
officialevsty <officialevsty@Ubuntu.myguest.virtualbox.org>
2025-02-18 13:13:12 +0300

    setup cookie envoy filters

--- src/api/auth/auth.js
+++ src/api/auth/auth.js
@@ -2,7 +2,7 @@ const { AuthServiceClient } = require('../../proto/sso_grpc_web_pb.js');
 const { RegisterRequest, LoginRequest,  RefreshTokenRequest, LogoutAllRequest, IsAdminRequest} = require('../../proto/sso_pb.js');
 
 // Auth client to communicate with sso auth service
-export const authClient = new AuthServiceClient('http://localhost:8081', null, null);
+export const authClient = new AuthServiceClient('http://localhost:8081', null, { withCredentials: true });
 
 // Creates a register request
 export const registerRequest = (email, password) => {
@@ -23,7 +23,7 @@ const loginRequest = (email, password, appId) => {
 
 const Login = (email, password, appId) => {
     const request = loginRequest(email, password, appId);
-    const metadata = { 'Content-Type': 'application/grpc-web' };
+    const metadata = { 'Content-Type': 'application/grpc-web'};
     authClient.login(request, metadata, (err, response) => {
         if (err) {
             console.error("Login error: "+ err);
